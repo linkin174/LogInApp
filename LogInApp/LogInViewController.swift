59e311f109a04ce02950df3afc140472b32c6255
Alex Kretov <kretov.1988@gmail.com>
2022-03-08 22:29:30 +0500

    Final Release

--- LogInApp/LogInViewController.swift
+++ LogInApp/LogInViewController.swift
@@ -7,43 +7,42 @@
 
 import UIKit
 
-class LogInViewController: UIViewController{
-    
+class LogInViewController: UIViewController {
     // MARK: IB Outlets
 
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
-    
+
     // MARK: Private properties
 
     private let username = "User"
     private let password = "password"
-    
+
     // MARK: Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         userNameTF.delegate = self
         passwordTF.delegate = self
-        
+
         setGradientBackGround(colors: [UIColor.systemMint, UIColor.cyan, UIColor.blue])
     }
-    
+
     // MARK: Override Methods
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
-    
+
     // MARK: Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let greetingVC = segue.destination as? WelcomeViewController else { return }
         greetingVC.username = userNameTF.text
     }
-    
+
     // MARK: IBActions
 
     @IBAction func reminderButtonsPressed(_ sender: UIButton) {
@@ -51,29 +50,36 @@ class LogInViewController: UIViewController{
             showAlert(title: "Your user name is", message: username, actionTitle: "Understand") :
             showAlert(title: "Your password is", message: password, actionTitle: "Cool")
     }
-    
+
     @IBAction func logInButtonPressed() {
-       if userNameTF.text == username && passwordTF.text == password {
-           performSegue(withIdentifier: "", sender: UIButton())
+        if userNameTF.text == username, passwordTF.text == password {
+            performSegue(withIdentifier: "", sender: UIButton())
         } else {
             showAlert(title: "ERROR", message: "Wrong user name or password", actionTitle: "Что ж")
             passwordTF.text?.removeAll()
         }
     }
-    
+
     @IBAction func unwind(segue: UIStoryboardSegue) {
         userNameTF.text?.removeAll()
         passwordTF.text?.removeAll()
         guard let greetingVC = segue.source as? WelcomeViewController else { return }
         greetingVC.dismiss(animated: true) {}
     }
-    
+
     // MARK: Private Methods
 
     private func showAlert(title: String, message: String, actionTitle: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(.init(title: actionTitle, style: .cancel))
         present(alert, animated: true)
+    }
+
+    private func setGradientBackGround(colors: [UIColor]) {
+        let gradient = CAGradientLayer()
+        gradient.frame = view.bounds
+        gradient.colors = colors.map { $0.cgColor }
+        view.layer.insertSublayer(gradient, at: 0)
     }
 }
 
@@ -89,12 +95,4 @@ extension LogInViewController: UITextFieldDelegate {
         }
         return true
     }
-    
-    private func setGradientBackGround(colors: [UIColor]) {
-        let gradient = CAGradientLayer()
-        gradient.frame = view.bounds
-        gradient.colors = colors.map({ $0.cgColor })
-        view.layer.insertSublayer(gradient, at: 0)
-    }
 }
-
--- LogInApp/WelcomeViewController.swift
+++ LogInApp/WelcomeViewController.swift
@@ -8,34 +8,28 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-    
     // MARK: Public Properties
-    
+
     var username: String!
-    
+
     // MARK: IBOutlets
-    
+
     @IBOutlet var greetingsLabel: UILabel!
-    
+
     // MARK: Life Cycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setGradientBackGround(colors: [UIColor.systemMint, UIColor.cyan, UIColor.blue])
         greetingsLabel.text! += " " + username + "!"
     }
-}
 
-//MARK: Extensions
-
-extension WelcomeViewController {
+    // MARK: Private methods
 
     private func setGradientBackGround(colors: [UIColor]) {
         let gradient = CAGradientLayer()
         gradient.frame = view.bounds
-        gradient.colors = colors.map({ $0.cgColor })
+        gradient.colors = colors.map { $0.cgColor }
         view.layer.insertSublayer(gradient, at: 0)
     }
 }
-
-
