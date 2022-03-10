ca455b4d584dd4fbaed123f2129745948100e928
Alex Kretov <kretov.1988@gmail.com>
2022-03-10 22:48:03 +0500

    Edited after review. Gradient needs to be simplified

--- LogInApp/LogInViewController.swift
+++ LogInApp/LogInViewController.swift
@@ -8,66 +8,58 @@
 import UIKit
 
 class LogInViewController: UIViewController {
+    
     // MARK: IB Outlets
-
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
+    
     //Constraints
     @IBOutlet var topSpacingConstraint: NSLayoutConstraint!
     @IBOutlet var loginButtonSpacingConstraint: NSLayoutConstraint!
 
     // MARK: Private properties
-
     private let username = "User"
     private let password = "password"
 
     // MARK: Life Cycle
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         registerKBNotifications()
-
+        setGradientBackGround(colors: [UIColor.systemMint, UIColor.cyan, UIColor.blue])
         userNameTF.delegate = self
         passwordTF.delegate = self
-
-        setGradientBackGround(colors: [UIColor.systemMint, UIColor.cyan, UIColor.blue])
     }
-
+    
+    // MARK: Initialization
     deinit {
         removeKBNotifications()
     }
 
-    // MARK: Override Methods
-
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        view.endEditing(true)
-    }
-
     // MARK: Navigation
-
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let greetingVC = segue.destination as! WelcomeViewController
-        greetingVC.username = userNameTF.text
-        greetingVC.modalPresentationStyle = .fullScreen
-        greetingVC.modalTransitionStyle = .flipHorizontal
+        guard let greetingVC = segue.destination as? WelcomeViewController else { return }
+        greetingVC.username = username
     }
 
     // MARK: IBActions
-
     @IBAction func reminderButtonsPressed(_ sender: UIButton) {
-        sender.tag == 0 ?
-            showAlert(title: "Your user name is", message: username, actionTitle: "Understand") :
-            showAlert(title: "Your password is", message: password, actionTitle: "Cool")
+        sender.tag == 0
+            ? showAlert(title: "Your user name is",
+                        message: username,
+                        actionTitle: "Understand")
+            : showAlert(title: "Your password is",
+                        message: password,
+                        actionTitle: "Cool")
     }
 
     @IBAction func logInButtonPressed() {
         if userNameTF.text == username, passwordTF.text == password {
             performSegue(withIdentifier: "toWelcomeScreen", sender: nil)
         } else {
-            showAlert(title: "ERROR", message: "Wrong user name or password", actionTitle: "Что ж")
-            passwordTF.text?.removeAll()
+            showAlert(title: "ERROR",
+                      message: "Wrong user name or password",
+                      actionTitle: "Что ж")
         }
     }
 
@@ -77,15 +69,7 @@ class LogInViewController: UIViewController {
     }
 
     // MARK: Private Methods
-
-    func showAlert(title: String, message: String, actionTitle: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: actionTitle, style: .cancel)
-        alert.addAction(action)
-        present(alert, animated: true)
-    }
-
-    func setGradientBackGround(colors: [UIColor]) {
+    private func setGradientBackGround(colors: [UIColor]) {
         let gradient = CAGradientLayer()
         gradient.frame = view.bounds
         gradient.colors = colors.map { $0.cgColor }
@@ -94,8 +78,60 @@ class LogInViewController: UIViewController {
 }
 
 // MARK: Extensions
+extension LogInViewController {
+    
+    private func showAlert(title: String, message: String, actionTitle: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: actionTitle, style: .cancel) { _ in
+            self.passwordTF.text?.removeAll()
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+
+    private func registerKBNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWasShown(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    private func removeKBNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc private func keyBoardWasShown(notification: NSNotification) {
+        
+        let info = notification.userInfo!
+        let offset = (info[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.size.height
+        
+        if UIScreen.main.bounds.maxY <= 568 {
+            loginButtonSpacingConstraint.constant = UIScreen.main.bounds.maxY / 15
+            topSpacingConstraint.constant = offset + 40
+            view.frame.origin = CGPoint(x: 0, y: -offset)
+        } else {
+            topSpacingConstraint.constant = offset * 1.6
+            loginButtonSpacingConstraint.constant = UIScreen.main.bounds.maxY / 18
+            view.frame.origin = CGPoint(x: 0, y: -offset)
+        }
+        UIView.animate(withDuration: 0.4) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc private func keyboardWasHidden(notification: NSNotification) {
+        topSpacingConstraint.constant = 150
+        loginButtonSpacingConstraint.constant = 60
+        view.frame.origin = CGPoint(x: 0, y: 0)
+        UIView.animate(withDuration: 0.4) {
+            self.view.layoutIfNeeded()
+        }
+    }
+}
 
 extension LogInViewController: UITextFieldDelegate {
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == userNameTF { // Проверка какое поле выбрано
             passwordTF.becomeFirstResponder() // Выбор следующего поля
@@ -104,43 +140,9 @@ extension LogInViewController: UITextFieldDelegate {
         }
         return true
     }
-
-    func registerKBNotifications() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWasShown(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-
-    func removeKBNotifications() {
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-
-    @objc func keyBoardWasShown(notification: NSNotification) {
-        let info = notification.userInfo!
-        let keyboardframe: CGRect = (info[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-        let offset = keyboardframe.size.height
-        
-        if view.bounds.maxY <= 568 {
-            loginButtonSpacingConstraint.constant = offset / 8
-            topSpacingConstraint.constant = offset + 40
-            view.frame.origin = CGPoint(x: 0, y: -offset)
-        } else {
-            topSpacingConstraint.constant = offset * 1.6
-            view.frame.origin = CGPoint(x: 0, y: -offset)
-        }
-
-        UIView.animate(withDuration: 0.4) {
-            self.view.layoutIfNeeded()
-        }
-    }
     
-    @objc func keyboardWasHidden(notification: NSNotification) {
-        topSpacingConstraint.constant = 150
-        loginButtonSpacingConstraint.constant = 60
-        view.frame.origin = CGPoint(x: 0, y: 0)
-        UIView.animate(withDuration: 0.4) {
-            self.view.layoutIfNeeded()
-        }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
 }
